81c8318a163a426b67e0e7e9d77a8e5020a294f0
Richmond Ampaabeng <richmondampaabeng@gmail.com>
2022-08-08 14:53:46 +0100

    Add second product infor and price

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
+const customer = {
+  firstname: "Kan",
+  lastName: "Led",
+  id: 3,
+  phoneNumber: "0740000000",
+  email: "gmail@gmail.co.uk",
+};
+customer.address = {
+  street: "NoahArk",
+  city: "London",
+  state: "Essex",
+  zipCode: "Rm10 8xa",
+};
 
+const productInformation = {
+  name: "",
+  price: 10,
+  description: "",
+  quantity: 20,
+};
+console.log(productInformation);
+
+const total = productInformation.price * 7;
