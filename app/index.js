ce249008e0643a2508844ebb2517e61853dc6214
Richmond Ampaabeng <richmondampaabeng@gmail.com>
2022-08-09 14:39:43 +0100

    Issue with using the rest tool

--- app/index.js
+++ app/index.js
@@ -1,163 +1,37 @@
-const couponcode = "jhghs";
-const couponcodeInput = "jhghs";
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 10,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 50,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 30,
+  },
+];
 
-if (couponcode === couponcodeInput) {
-  console.log("total - 70 ");
-} else {
-  console.log("No discount !");
-}
+const [, ...last2Products] = products;
 
-console.log("msg");
-const customer = {
-  firstname: "Kan",
-  lastName: "Led",
-  id: 3,
-  phoneNumber: "0740000000",
-  email: "gmail@gmail.co.uk",
-};
+last2Products.push({
+  id: 4,
+  name: "Product 4",
+  price: 40,
+});
 
-// I got confused as I had console log above and below each object containing variables
+console.log(products, last2Products);
 
-customer.address = {
-  street: "NoahArk",
-  city: "London",
-  state: "Essex",
-  zipCode: "Rm10 8xa",
-};
+last2Products[0].name = "Product 2 updated";
 
-console.log("Welcome, Kan Led!");
+console.log(products);
 
-const productInformation = {
-  name: "Kan",
-  price: 10,
-  description: "adaptable",
-  quantity: 70,
-};
-console.log(productInformation.quantity);
+// why did the product 2 update to 'product 2 updated'
 
-let quantity = 1000;
+// throwing the objects in the, a mutation was performed and it has updated it. this is why we avoid mutations and use the SPREAD OPERATOR for correct it.
 
-const total = productInformation.price * 7;
-
-productInformation.quantity -= 7;
-
-if (total >= 500) {
-  console.log("You get free extra product");
-}
-
-// this is implicit coercion
-if (productInformation.quantity) {
-  console.log("We have stock");
-} else {
-  console.log("We do not have it");
-}
-
-// any non-empty string is truthy
-
-const someInputFromAForm = "Mark";
-
-const name = someInputFromAForm || "Anonymous";
-
-console.log(someInputFromAForm);
-
-// Practicing ternaries
-
-const moneyIhave = 75;
-const haveSavings = false;
-
-const msg =
-  moneyIhave > 100 || haveSavings
-    ? console.log("Yay! Ice cream!")
-    : console.log("Ugh! Lima beans!");
-
-console.log("msg");
-
-// here msg is the variable
-
-// Switch
-const choice = "Lasagne";
-
-switch (choice) {
-  case "Jollof":
-    console.log("Jollof rice!");
-    break;
-  case "Lasagne":
-    console.log("Italian food!");
-    break;
-  case "Pulled beef":
-    console.log("Pulled beef wrap!");
-    break;
-  default:
-    console.log("We don't have that food.");
-}
-
-// Declaring arrays
-const nums = [10, 6, 8, 4, 15, 21];
-const colours = ["green", "orange", "purple"];
-
-const col = colours[0];
-console.log(col);
-
-const num = nums[0];
-const num2 = nums[4];
-
-// so mine my 0 index is the number 10 and when nums is 4 (index), this is equivalent to 15
-console.log(num);
-
-const nums2 = [4, 11, 16];
-const nums3 = nums.concat(nums2);
-
-console.log(nums2);
-console.log(nums3);
-
-// Object Chaining: undefined
-
-const me = {
-  name: "BuzzLightyear",
-  age: 400,
-};
-
-console.log(me.handles);
-
-const fname = "Mark";
-const lname = "Galloway";
-const status = "retired";
-
-const wrestler = {
-  fname,
-  lname,
-  status,
-};
-
-const personName = wrestler.status?.fname;
-console.log(personName);
-console.log(wrestler);
-console.log(fname.lname);
-
-const me2 = {
-  identifier: "CodeFinity",
-  age: 22,
-  mileTime: "3:55",
-  maxBench: 500,
-  isLying: true,
-};
-
-const { identifier, age, mileTime } = me2;
-
-const { name: handle, age: marsyears } = me2;
-
-console.log(handle, marsyears);
-console.log(me2);
-
-// Array deconstruction and Rest
-const car = ["toyota", "mazda", "vauxhall", "bentley"];
-
-const [toyota, ...otherCar] = car;
-
-otherCar[100] = "jaguar";
-
-const car2 = car;
-
-car2.push("honda");
-console.log(car, otherCar);
-console.log(car, car2);
+// jUST BECAUSE YOU USED THE REST OPERATOR DOES NOT MEAN YOU ARE SAFE FORM MUTATIONS
